bd90acc8296a76dda5993aa497e278fd45293816
Kaifa <kaifamandal1994@gmail.com>
2023-10-05 22:51:10 +0530

    Upadted codes along with new problems

--- PrepJava/src/learning/linkedlist/Runner.java
+++ PrepJava/src/learning/linkedlist/Runner.java
@@ -1,4 +1,4 @@
-package learning.linkedlist;
+package PrepJava.src.learning.linkedlist;
 
 
 public class Runner extends myLinkedList {
@@ -7,23 +7,26 @@ public class Runner extends myLinkedList {
 
 		myLinkedList l1 = new myLinkedList();
 		l1.add(2);
-		l1.add(2);
-		l1.add(3);
+		l1.add(6);
 		l1.add(4);
-		l1.add(4);
-		//l1.print();
-		
-		removeDup(l1.head);
+		l1.add(11);
+		l1.add(9);
 		l1.print();
-	}
-
-	public static Node removeDup(Node head) {
 		
-		if(head==null||head.next==null) {
-			return head;
-		}
-		head.next=removeDup(head.next);
-		return (head.data==head.next.data)?head.next:head;
+		//removeDup(l1.head);
+		System.out.println("\n");
+		//l1.head=l1.revLinkedList(l1.head);
+		l1.head=l1.revLnkLstWREC(l1.head);
+		l1.print();
+
+
+//	public static Node removeDup(Node head) {
+//
+//		if(head==null||head.next==null) {
+//			return head;
+//		}
+//		head.next=removeDup(head.next);
+//		return (head.data==head.next.data)?head.next:head;
 		}
 	}
 
--- PrepJava/src/learning/linkedlist/testLinkfunc.java
+++ PrepJava/src/learning/linkedlist/testLinkfunc.java
@@ -1,4 +1,4 @@
-package learning.linkedlist;
+package PrepJava.src.learning.linkedlist;
 
 import java.util.ArrayList;
 import java.util.Arrays;
