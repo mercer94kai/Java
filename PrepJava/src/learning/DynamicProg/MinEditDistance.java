9c7ace7a7697da5b178807c0b2b65877a96c9984
kamandal <kaifamandal@1994@gmail.com>
2024-09-25 18:28:12 +0530

    GFG code Practices

--- PrepJava/src/learning/DynamicProg/MinEditDistance.java
+++ PrepJava/src/learning/DynamicProg/MinEditDistance.java
@@ -11,10 +11,10 @@ public class MinEditDistance {
         int n=b.length();
 
         int[][] dp = new int[m+1][n+1];
-        for (int i=1;i<=m;i++){
+        for (int i=0;i<=m;i++){
             dp[i][0]=i;
         }
-        for (int j=1;j<=n;j++){
+        for (int j=0;j<=n;j++){
             dp[0][j]=j;
         }
 
